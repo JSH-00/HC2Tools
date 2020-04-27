8030482fe1d83e391769586d73cbf49ba500c2c2
JSH-00 <jshjshh@163.com>
2020-04-27 10:24:55 +0800

    add done information

--- set_password.py
+++ set_password.py
@@ -3,9 +3,6 @@
 import sys, getopt
 import os
 
-# print ("飞机信息：")
-# os.system("adb shell \"cat data/misc/wifi/hostapd.conf | grep ^ssid= | sed 's/ssid=/SSID：/g'\"")
-# os.system("adb shell \"cat data/misc/wifi/hostapd.conf | grep wpa_passphrase= | sed 's/wpa_passphrase=/Password：/g'\"")
 def setPassSSID(arg):
     print ("this is setPassSSID")
     os.system("adb shell \"sed -i 's/^ssid=.*/ssid=%s/g' data/misc/wifi/hostapd.conf\""%(arg[2]))
@@ -24,13 +21,11 @@ def setSSID(arg):
     os.system("adb shell \"sed -i 's/^ssid=.*/ssid=%s/g' data/misc/wifi/hostapd.conf\"" % (arg[2]))
 
 def getHelp():
-        print("设置SSID和密码：python XXX.py -sSSID new_ssid -sPASS new_password")
-        print("设置SSID：python XXX.py -sSSID new_ssid")
-        print("设置密码：python XXX.py -sPASS new_password")
-        print("获取飞机信息：python XXX.py -g")
-        print("帮助文档：python XXX.py -h")
-
-
+    print("设置SSID和密码：python XXX.py -sSSID new_ssid -sPASS new_password")
+    print("设置SSID：python XXX.py -sSSID new_ssid")
+    print("设置密码：python XXX.py -sPASS new_password")
+    print("获取飞机信息：python XXX.py -g")
+    print("帮助文档：python XXX.py -h")
 
 if  sys.argv[1]=="-sSSID" and sys.argv[3]=="-sPASS":
     setPassSSID(sys.argv)
@@ -41,11 +36,12 @@ elif sys.argv[1]=="-sPASS":
 elif sys.argv[1] == "-sSSID":
     setPass(sys.argv)
     setSuccess()
-    # print (len(sys.argv))
 elif sys.argv[1] == "-g":
     print ("飞机信息：")
     os.system("adb shell \"cat data/misc/wifi/hostapd.conf | grep ^ssid= | sed 's/ssid=/SSID：/g'\"")
     os.system("adb shell \"cat data/misc/wifi/hostapd.conf | grep wpa_passphrase= | sed 's/wpa_passphrase=/Password：/g'\"")
+    os.system("adb shell \"getprop zz.product.version\"")
+    os.system("adb shell \"opkg info | grep Version: | sed 's/Version:/IPK：/g'\"")
 elif sys.argv[1] == "-h":
     getHelp()
 else:
