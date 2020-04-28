837c8a745061e4d7c5bf282eb28179569a2b4d4d
jishuhan <jishuhan@zerozero.cn>
2020-04-28 15:25:52 +0800

    add done information

--- set_password.py
+++ set_password.py
@@ -3,10 +3,13 @@
 import sys, getopt
 import os
 
-def setPassSSID(arg):
-    print ("this is setPassSSID")
-    os.system("adb shell \"sed -i 's/^ssid=.*/ssid=%s/g' data/misc/wifi/hostapd.conf\""%(arg[2]))
-    os.system("adb shell \"sed -i 's/wpa_passphrase=.*/wpa_passphrase=%s/g' data/misc/wifi/hostapd.conf\""%(arg[4]))
+def setPassSSID(arg_pass, arg_ssid):
+    setPass(arg_pass)
+    setSSID(arg_ssid)
+
+def setSSIDPass(arg_ssid, arg_pass):
+    setSSID(arg_ssid)
+    setPass(arg_pass)
 
 def setSuccess():
     print ("更改成功!!")
@@ -14,23 +17,27 @@ def setSuccess():
     os.system("adb shell \"cat data/misc/wifi/hostapd.conf | grep wpa_passphrase= | sed 's/wpa_passphrase=/Password：/g'\"")
     os.system("adb reboot -f")
 
-def setPass(arg):
-    os.system("adb shell \"sed -i 's/wpa_passphrase=.*/wpa_passphrase=%s/g' data/misc/wifi/hostapd.conf\"" % (arg[2]))
+def setPass(arg_pass):
+    os.system("adb shell \"sed -i 's/wpa_passphrase=.*/wpa_passphrase=%s/g' data/misc/wifi/hostapd.conf\"" % arg_pass)
 
-def setSSID(arg):
-    os.system("adb shell \"sed -i 's/^ssid=.*/ssid=%s/g' data/misc/wifi/hostapd.conf\"" % (arg[2]))
+def setSSID(arg_ssid):
+    os.system("adb shell \"sed -i 's/^ssid=.*/ssid=%s/g' data/misc/wifi/hostapd.conf\"" % arg_ssid)
 
 def getHelp():
     print("设置SSID和密码：python XXX.py -sSSID new_ssid -sPASS new_password")
+    print("设置密码和SSID：python XXX.py -sPASS new_password -sSSID new_ssid")
     print("设置SSID：python XXX.py -sSSID new_ssid")
     print("设置密码：python XXX.py -sPASS new_password")
     print("获取飞机信息：python XXX.py -g")
     print("帮助文档：python XXX.py -h")
 
-if  sys.argv[1]=="-sSSID" and sys.argv[3]=="-sPASS":
-    setPassSSID(sys.argv)
+if  sys.argv[1] == "-sSSID" and sys.argv[3] == "-sPASS":
+    setPassSSID(sys.argv[2], sys.argv[4])
     setSuccess()
-elif sys.argv[1]=="-sPASS":
+elif sys.argv[1] == "-sPASS" and sys.argv[3] == "-sSSID":
+    setSSIDPass(sys.argv[2], sys.argv[4])
+    setSuccess()
+elif sys.argv[1] == "-sPASS":
     setPass(sys.argv)
     setSuccess()
 elif sys.argv[1] == "-sSSID":
