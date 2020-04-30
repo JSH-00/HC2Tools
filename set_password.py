4057748bd2b5d7f27a81580290f4c9c9b718f8a8
jishuhan <jishuhan@zerozero.cn>
2020-04-30 08:52:33 +0800

    getopt 更新

--- set_password.py
+++ set_password.py
@@ -3,81 +3,67 @@
 import sys, getopt
 import os
 
-def setPassSSID(arg_pass, arg_ssid):
-    setPass(arg_pass)
-    setSSID(arg_ssid)
+def setSSID(new_ssid):
+    os.system("adb shell \"sed -i 's/^ssid=.*/ssid=%s/g' data/misc/wifi/hostapd.conf\"" % new_ssid)
 
-def setSSIDPass(arg_ssid, arg_pass):
-    setSSID(arg_ssid)
-    setPass(arg_pass)
-
-def setPass(arg_pass):
-    os.system("adb shell \"sed -i 's/wpa_passphrase=.*/wpa_passphrase=%s/g' data/misc/wifi/hostapd.conf\"" % arg_pass)
-
-def setSSID(arg_ssid):
-    os.system("adb shell \"sed -i 's/^ssid=.*/ssid=%s/g' data/misc/wifi/hostapd.conf\"" % arg_ssid)
+def setPass(new_pass):
+    os.system("adb shell \"sed -i 's/wpa_passphrase=.*/wpa_passphrase=%s/g' data/misc/wifi/hostapd.conf\"" % new_pass)
 
 def printSSIDPass():
     os.system("adb shell \"cat data/misc/wifi/hostapd.conf | grep ^ssid= | sed 's/ssid=/SSID：/g'\"")
     os.system("adb shell \"cat data/misc/wifi/hostapd.conf | grep wpa_passphrase= | sed 's/wpa_passphrase=/Password：/g'\"")
 
-def setSuccess():
-    print ("更改成功!")
-    printSSIDPass()
+def setSuccessReboot():
+    print ("更改成功,正在重启……")
     os.system("adb reboot -f")
 
 def setFailed():
     print ("运行失败,-h 查看帮助文档")
 
-def getHelp():
-    print("设置SSID和密码：python XXX.py -sSSID new_ssid -sPASS new_password")
-    print("设置密码和SSID：python XXX.py -sPASS new_password -sSSID new_ssid")
-    print("设置SSID：python XXX.py -sSSID new_ssid")
-    print("设置密码：python XXX.py -sPASS new_password")
-    print("获取飞机信息：python XXX.py -g")
-    print("帮助文档：python XXX.py -h")
-
-if len(sys.argv) == 5:
-    if sys.argv[1] == "-sSSID" and sys.argv[3] == "-sPASS":
-        print (len(sys.argv))
-        setSSIDPass(sys.argv[2], sys.argv[4])
-        setSuccess()
-    elif sys.argv[1] == "-sPASS" and sys.argv[3] == "-sSSID":
-        setPassSSID(sys.argv[2], sys.argv[4])
-        setSuccess()
-    else:
-        setFailed()
-elif len(sys.argv) == 3:
-    if sys.argv[1] == "-sPASS":
-        print ("setPASS")
-        setPass(sys.argv[2])
-        setSuccess()
-    elif sys.argv[1] == "-sSSID":
-        setSSID(sys.argv[2])
-        setSuccess()
-    else:
-        setFailed()
-elif len(sys.argv) == 2:
-    if sys.argv[1] == "-g":
+def getInfo():
         print ("飞机信息：")
-        os.system("adb shell \"cat data/misc/wifi/hostapd.conf | grep ^ssid= | sed 's/ssid=/SSID：/g'\"")
-        os.system(
-            "adb shell \"cat data/misc/wifi/hostapd.conf | grep wpa_passphrase= | sed 's/wpa_passphrase=/Password：/g'\"")
+        printSSIDPass()
         os.system("adb shell \"getprop zz.product.version\"")
         os.system("adb shell \"opkg info | grep Version: | sed 's/Version:/IPK：/g'\"")
-    elif sys.argv[1] == "-h":
-        getHelp()
-    else:
-        setFailed()
-else:
+
+def getHelp():
+    print("设置SSID：-s new_ssid 或 --sSSID new_ssid 或 --sSSID=new_ssid\n"
+          "设置密码：-p new_pass 或 --sPASS new_password 或 --sPASS=new_password\n"
+          "获取飞机信息：-g 或 --gINFO\n"
+          "帮助文档：-h 或 --help")
+set_ok = 0 # 定义返回设置/获取状态参数，判断是否设置/获取成功、是否需要重启
+opts,args = getopt.gnu_getopt(sys.argv[1:],'-s:-p:-g-h',['sSSID=','sPASS=','gINFO','help'])
+
+# 输入有冗余判断（只判断冗余的普通字符串，输入“-/--”开头的，getopt自身会判断并报错）
+if args:
     setFailed()
+    exit()
 
+for opt_name,opt_values in opts:
 
-# os.system("adb shell \"cat data/misc/wifi/hostapd.conf | grep ^ssid=\"")
-# os.system("adb shell \"cat data/misc/wifi/hostapd.conf | grep wpa_passphrase=\"")
-# os.system("adb shell \"sed -i 's/^ssid=.*/ssid=Hover_JSH/g' data/misc/wifi/hostapd.conf\"")
-# os.system("adb shell \"sed -i 's/wpa_passphrase=.*/wpa_passphrase=0987654321/g' data/misc/wifi/hostapd.conf\"")
-# os.system("adb shell \"cat data/misc/wifi/hostapd.conf | grep ^ssid=\"")
-# os.system("adb shell \"cat data/misc/wifi/hostapd.conf | grep wpa_passphrase=\"")
+    if opt_name in ('-s','--sSSID'):
+        newSSID = opt_values
+        setSSID(newSSID)
+        set_ok += 4
+    if opt_name in ('-p','--sPASS'):
+        newPASS = opt_values
+        setPass(newPASS)
+        set_ok += 4
+    if opt_name in ('-g', '--gINFO'):
+        getInfo()
+        set_ok += 1
+    if opt_name in ('-h', '--help'):
+        getHelp()
+        set_ok += 1
+print(set_ok)
+
+# 判断是否设置/获取成功并要重启
+if set_ok > 3:
+    printSSIDPass()
+    setSuccessReboot()
+elif set_ok > 0:
+    print ("获取成功")
+else:
+    setFailed() # 未输入命令行参数
 
 # ssid=Hover_2-C2BD66
